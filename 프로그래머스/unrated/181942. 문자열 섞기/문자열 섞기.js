2284ed56a41d564fa750c3e716f40fa80822eab3
오소현 <53892427+osohyun0224@users.noreply.github.com>
2023-07-11 15:45:00 +0900

    [unrated] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,3 +1,7 @@
 function solution(str1, str2) {
-    return [...str1].map((a,i) => a+str2[i]).join("");
+    var answer = '';
+    for( let i =0 ; i<str1.length;i++){
+        answer+= str1[i]+str2[i];
+    }
+    return answer;
 }
